dc498e4998046a6e076b47026e35130b8cf5bb0c
Roman <romabukhtiarov@gmail.com>
2019-02-09 16:41:07 +0300

    Add custom error processing

--- demo/src/main/java/com/deltabook/demo/DemoApplication.java
+++ demo/src/main/java/com/deltabook/demo/DemoApplication.java
@@ -2,12 +2,15 @@ package com.deltabook.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("It works!");
+        System.out.println("It works!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
