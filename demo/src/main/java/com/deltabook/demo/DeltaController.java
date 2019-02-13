197dbde82724000e245a0b237dec44f0b2c0e560
Roman <romabukhtiarov@gmail.com>
2019-02-13 06:49:48 +0300

    Provide reg data user input to DB (#11)

--- demo/src/main/java/com/deltabook/demo/DeltaController.java
+++ demo/src/main/java/com/deltabook/demo/DeltaController.java
@@ -1,18 +1,33 @@
 package com.deltabook.demo;
 
+import com.deltabook.demo.model.User;
+import com.deltabook.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DeltaController {
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/")
     public String MainPage() {
         return "main";
     }
 
     @RequestMapping("/registration")
-    public String Registration() {
+    public String Registration(Model model) {
+        model.addAttribute("objectToFill", new User ());
         return "registration";
     }
 
+    @RequestMapping( value = "/enter_reg", method = RequestMethod.POST)
+    String sum(@ModelAttribute User insertedObject, Model model) {
+        userRepository.save(new User(insertedObject.getLogin(), insertedObject.getPassword()));
+        return "main";
+    }
 }
--- demo/src/main/java/com/deltabook/demo/model/User.java
+++ demo/src/main/java/com/deltabook/demo/model/User.java
@@ -3,13 +3,76 @@ package com.deltabook.demo.model;
 import javax.persistence.*;
 import java.util.List;
 
+import static com.deltabook.demo.model.Roles.ROLE_USER;
+
 @Entity
 @Table(name = "users")
 public class User {
-
+    public User() {}
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+        this.role = ROLE_USER;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 
     private String login;
     private Roles role;
