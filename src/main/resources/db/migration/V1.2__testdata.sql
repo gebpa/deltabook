f2e4aea266bd6497d604e125b04d95fbef06290e
Roman <romabukhtiarov@gmail.com>
2019-02-14 22:28:29 +0300

    Update sql to match spring security changes (#26)

--- src/main/resources/db/migration/V1.2__testdata.sql
+++ src/main/resources/db/migration/V1.2__testdata.sql
@@ -1,8 +1,13 @@
-INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('admin2', 0, 'admin', 'ivan', 'ivanov',false, false);
-INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('nagibator98', 1, 'admin98', 'dmitry', 'sidorov',false, false);
-INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('dd04072001', 1, 'alex0407', 'alex', 'wolf',false, false);
-INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('fedor_traktorist', 1, 'fedor78', 'fedor', 'ignatev',false, false);
-INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('selo78', 1, 'chichik78', 'anton', 'chichikov',false, false);
+--PASSWORD: admin2
+INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('admin2', 0, '$2a$10$Ww8xw7pyohtuWgka0rMNFuRb6OpVHgWvo/Xd.8YvCzM3.l7LKaawe', 'ivan', 'ivanov',false, false);
+-- admin98
+INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('nagibator98', 1, '$2a$10$cWUZjdgQUwwz8bsblA9w4uhqyAz5/VR8P.2j7XTvbRXan4NU7iD62', 'dmitry', 'sidorov',false, false);
+-- alex0407
+INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('dd04072001', 1, '$2a$10$SzA4Tj/t5Us3w/3ZGmIM0u0uEASvGoL9TiCa87mzquu3Pkb2.RT5e', 'alex', 'wolf',false, false);
+-- fedor78
+INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('fedor_traktorist', 1, '$2a$10$LU0euh4a.M14VCNQ.XI4KeAbkZvCGf7lCdFkmbMaYJI6xjIAifEBm', 'fedor', 'ignatev',false, false);
+-- chichik78
+INSERT INTO USERS (LOGIN, ROLE, PASSWORD, FIRST_NAME, LAST_NAME, is_Blocked,is_Deleted ) VALUES ('selo78', 1, '$2a$10$DJH86Chgp.1HSJUsfivod.RjJ9DgAq/k/ind1X0b3RY2DCMNt5K/C', 'anton', 'chichikov',false, false);
 
 INSERT INTO CONTACTS(FRIEND_FROM_ID, FRIEND_TO_ID, IS_ACCEPTED, REQUEST_MESSAGE) VALUES (1, 2, false, 'приффки');
 INSERT INTO CONTACTS(FRIEND_FROM_ID, FRIEND_TO_ID, IS_ACCEPTED, REQUEST_MESSAGE) VALUES (2, 3, false, 'хей чувак');
