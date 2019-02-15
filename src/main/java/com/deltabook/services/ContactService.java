4a05244e935170ba02478b118237008d9ce0838c
LyubaPetrova <lyuba_petrova_1998@mail.ru>
2019-02-15 04:30:20 +0300

    изменен дизайн страницы main

---
+++ src/main/java/com/deltabook/services/ContactService.java
@@ -0,0 +1,15 @@
+package com.deltabook.services;
+
+import com.deltabook.model.Contact;
+import com.deltabook.model.User;
+
+import java.util.List;
+
+public interface ContactService {
+    String sendRequestFriend(User fromUser, User toUser, String requestMessage);
+    List<Contact> getAllRequestsFromUser(User user);
+    List<Contact> getAllRequestsToUser(User user);
+    List<Contact> getAllFriends(User user);
+    void confirmRequest(User fromUser, User toUser);
+    void declineRequest(User fromUser, User toUser);
+}
