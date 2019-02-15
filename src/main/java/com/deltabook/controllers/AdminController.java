e93697a51e10e0ad02084e8a386488b4a5d28c66
gleb <gleber1995@gmail.com>
2019-02-15 15:25:30 +0300

    pull request version

--- src/main/java/com/deltabook/controllers/AdminController.java
+++ src/main/java/com/deltabook/controllers/AdminController.java
@@ -3,6 +3,7 @@ package com.deltabook.controllers;
 import com.deltabook.model.User;
 import com.deltabook.model.send.SendChangeUser;
 import com.deltabook.security.details.UserDetailsImpl;
+import com.deltabook.services.UserService;
 import com.deltabook.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -13,44 +14,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
-    @Autowired
-    private UserServiceImpl UserServiceImpl;
-
-    @RequestMapping("/main_admin")
-    public String mainAdmin(Authentication authentication, Model model) {
-        model.addAttribute("SendChangeUser", new SendChangeUser());
-        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
-        User user = principal.getUser();
-        switch(user.getRole()) {
-            case ROLE_USER: return ("/");
-            case ROLE_ADMIN: return("/main_admin");
-        }
-        return "main_admin";
-    }
-    @RequestMapping("/change_user_last_name")
-    public String changeUserLastName(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser) {
-        User user = UserServiceImpl.getUserByLogin(SendChangeUser.getNickName());
-        user.setLastName(SendChangeUser.getNewLastName());
-        UserServiceImpl.SaveUser(user);
-        model.addAttribute("SendChangeUser", new SendChangeUser());
-        return "main_admin";
-    }
-    @RequestMapping("/delete_user")
-    public String deleteUser(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser, @RequestParam(value="action", required=true) String action) {
-        User user = UserServiceImpl.getUserByLogin(SendChangeUser.getNickName());
-        switch(action) {
-            case "total_delete": {
-                UserServiceImpl.deleteUser(user);
-                break;
-            }
-            case "temp_delete":  {
-                user.setDeleted(true);
-                UserServiceImpl.SaveUser(user);
-                break;
-            }
-            default: break;
-        }
-        model.addAttribute("SendChangeUser", new SendChangeUser());
-        return "main_admin";
-    }
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @RequestMapping("/main_admin")
+//    public String mainAdmin(Authentication authentication, Model model) {
+//        model.addAttribute("SendChangeUser", new SendChangeUser());
+//        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+//        User user = principal.getUser();
+//        switch(user.getRole()) {
+//            case ROLE_USER: return ("/");
+//            case ROLE_ADMIN: return("/main_admin");
+//        }
+//        return "main_admin";
+//    }
+//    @RequestMapping("/change_user_last_name")
+//    public String changeUserLastName(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser) {
+//        User user = userService.getUserByLogin(SendChangeUser.getNickName());
+//        user.setLastName(SendChangeUser.getNewLastName());
+//        UserServiceImpl.SaveUser(user);
+//        model.addAttribute("SendChangeUser", new SendChangeUser());
+//        return "main_admin";
+//    }
+//    @RequestMapping("/delete_user")
+//    public String deleteUser(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser, @RequestParam(value="action", required=true) String action) {
+//        User user = UserServiceImpl.getUserByLogin(SendChangeUser.getNickName());
+//        switch(action) {
+//            case "total_delete": {
+//                UserServiceImpl.deleteUser(user);
+//                break;
+//            }
+//            case "temp_delete":  {
+//                user.setDeleted(true);
+//                UserServiceImpl.SaveUser(user);
+//                break;
+//            }
+//            default: break;
+//        }
+//        model.addAttribute("SendChangeUser", new SendChangeUser());
+//        return "main_admin";
+//    }
 }
